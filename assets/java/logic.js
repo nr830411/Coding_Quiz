5cf7523134a9f65cbbc3b54b5d79ebdd65f451fc
Nico Reed <nr830411@gmail.com>
2019-12-03 16:18:55 -0500

    (added onClick event to hide start page)

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -3,8 +3,8 @@ var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 //
-var questions = document.querySelector("#questions");
-var timer = document.querySelector("#time");
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
 var choicesEl = document.querySelector("#choices");
 var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
@@ -17,12 +17,12 @@ function startQuiz() {
   startScreen.setAttribute("class", "hide");
 
   
-  questions.removeAttribute("class");
+  questionsEl.removeAttribute("class");
 
 // Starts Quiz
   timerId = setInterval(clockTick, 1000);
 
-  timer.textContent = time;
+  timerEl.textContent = time;
 
   getQuestion();
 }
@@ -30,7 +30,7 @@ function startQuiz() {
 function getQuestion() {
 
   var currentQuestion = questions[currentQuestionIndex];
-  
+
 // update title with current question
 var titleEl = document.querySelector("#question-title");
 titleEl.textContent = currentQuestion.title;
@@ -52,4 +52,41 @@ currentQuestion.choices.forEach(function(choice, i) {
 });
 }
 
+function questionClick() {
+  // check if user guessed wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    // display new time on page
+    timerEl.textContent = time;
+
+    feedbackEl.textContent = "Wrong!";
+  } else {
+    feedbackEl.textContent = "Correct!";
+  }
+
+  // flash right/wrong feedback on page for half a second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  // move to next question
+  currentQuestionIndex++;
+
+  // check if we've run out of questions
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+//STARTS QUIZ
+startBtn.onclick = startQuiz;
 
